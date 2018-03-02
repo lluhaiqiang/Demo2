419959838299b5d7db47c80990f0fd14c438bd9f
lhq <lhq@gmail.com>
2018-03-02 16:48:24 +0800

    three commit.

--- app/src/main/java/lhq/example/com/demo/MainActivity.java
+++ app/src/main/java/lhq/example/com/demo/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //username password:1234567
+        //username password:1114146
     }
 }
