1e98e46136a5151ce069531b4a0d012c32d11bba
lhq1135740561 <lluhaiqiang@163.com>
2018-03-02 15:44:09 +0800

    username change2

--- app/src/main/java/lhq/example/com/demo/MainActivity.java
+++ app/src/main/java/lhq/example/com/demo/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //username password:123456
+        //username password:1234567
     }
 }
